c2d7288c421f2fb988e09f92ceca797a8c704b96
Sandesh Verma <sandeshv414@gmail.com>
2022-05-02 16:44:18 +0530

    update2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cwh-todo-list';
+  title = 'TODO LIST';
   constructor(){
-  //   setTimeout(() => {
-  //     this.title = "Changed title";
-  // }, 2000);
   
   }
 }
